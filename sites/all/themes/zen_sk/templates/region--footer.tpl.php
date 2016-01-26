a2d7d615043477026b40e4032235a07b5a8bdff2
albert <alberta@zylun.com>
2016-01-26 17:22:18 +0800

    first commit

---
+++ sites/all/themes/zen_sk/templates/region--footer.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for the footer region.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728140
+ */
+?>
+<?php if ($content): ?>
+  <footer id="footer" class="<?php print $classes; ?>">
+    &copy; All Rights Reseved || Treby Alinson
+  </footer>
+<?php endif; ?>
